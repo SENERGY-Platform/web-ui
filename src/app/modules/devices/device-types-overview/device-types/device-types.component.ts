213a1b4d49ee22bb399b5e1b34c03a0ef342e9e2
fjauer <19175201+fjauer@users.noreply.github.com>
2019-08-01 11:43:37 +0200

    stringify content_variable to content_variable_raw in device-types.component

--- src/app/modules/devices/device-types-overview/device-types/device-types.component.ts
+++ src/app/modules/devices/device-types-overview/device-types/device-types.component.ts
@@ -424,7 +424,7 @@ export class DeviceTypesComponent implements OnInit {
             id: [content.id],
             name: [protocolSegment.name],
             serialization: [content.serialization],
-            content_variable_raw: [content.content_variable, jsonValidator(true)],
+            content_variable_raw: [JSON.stringify(content.content_variable), jsonValidator(true)],
             protocol_segment_id: [content.protocol_segment_id],
             show: [content.protocol_segment_id ? true : false],
         });
