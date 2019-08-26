80c83a07536cab497b534b104a634da7355f1693
TZ <zsco@users.noreply.github.com>
2019-08-26 15:39:54 +0200

    add deployment field selection to analytics operator config dialogs

--- src/app/modules/data/operator-repo/shared/operator.model.ts
+++ src/app/modules/data/operator-repo/shared/operator.model.ts
@@ -20,6 +20,7 @@ export interface OperatorModel {
     image: string | undefined;
     description: string | undefined;
     pub: boolean | undefined;
+    deploymentType: boolean | undefined;
     userId: string | undefined;
     inputs: IOModel [] | undefined;
     outputs: IOModel [] | undefined;
