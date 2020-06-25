7d15256131b246a11f3272e1c0d155a7b0dabead
fjauer <19175201+fjauer@users.noreply.github.com>
2020-06-25 17:12:36 +0200

    add missing imports for dashboard.component test

--- src/app/modules/dashboard/dashboard.component.spec.ts
+++ src/app/modules/dashboard/dashboard.component.spec.ts
@@ -38,6 +38,10 @@ import {DashboardModel} from './shared/dashboard.model';
 import {DashboardManipulationModel} from './shared/dashboard-manipulation.model';
 import {DashboardWidgetManipulationModel} from './shared/dashboard-widget-manipulation.model';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatDividerModule} from '@angular/material/divider';
+import {GridsterModule} from 'angular-gridster2';
+import {MatButtonModule} from '@angular/material/button';
 
 
 describe('DeviceStatusEditDialogComponent', () => {
@@ -69,7 +73,11 @@ describe('DeviceStatusEditDialogComponent', () => {
                 MatExpansionModule,
                 MatInputModule,
                 MatMenuModule,
-                ReactiveFormsModule
+                ReactiveFormsModule,
+                MatTabsModule,
+                MatDividerModule,
+                GridsterModule,
+                MatButtonModule,
             ],
             declarations: [
                 DashboardComponent
