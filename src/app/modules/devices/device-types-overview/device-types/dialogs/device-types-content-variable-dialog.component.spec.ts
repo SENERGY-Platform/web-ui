36001884c62b211933003588ad6c3b0ee729339c
fjauer <19175201+fjauer@users.noreply.github.com>
2020-07-03 11:16:12 +0200

    SNRGY-750 refactor device-types-content-variable-dialog and fix, add tests

--- src/app/modules/devices/device-types-overview/device-types/dialogs/device-types-content-variable-dialog.component.spec.ts
+++ src/app/modules/devices/device-types-overview/device-types/dialogs/device-types-content-variable-dialog.component.spec.ts
@@ -18,9 +18,13 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {DeviceTypesContentVariableDialogComponent} from './device-types-content-variable-dialog.component';
 import {CoreModule} from '../../../../../core/core.module';
 import {createSpyFromClass, Spy} from 'jasmine-auto-spies';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {ConceptsService} from '../../../concepts/shared/concepts.service';
 import {DeviceTypeContentVariableModel, DeviceTypeFunctionModel} from '../../shared/device-type.model';
+import {MatRadioModule} from '@angular/material/radio';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 
 fdescribe('DeviceTypesContentVariableDialog', () => {
     let component: DeviceTypesContentVariableDialogComponent;
@@ -29,26 +33,142 @@ fdescribe('DeviceTypesContentVariableDialog', () => {
     const matDialogRefSpy: Spy<MatDialogRef<DeviceTypesContentVariableDialogComponent>> = createSpyFromClass<MatDialogRef<DeviceTypesContentVariableDialogComponent>>(MatDialogRef);
     const conceptServiceSpy: Spy<ConceptsService> = createSpyFromClass(ConceptsService);
 
-    beforeEach(async(() => {
+    function init(contentVariable: DeviceTypeContentVariableModel) {
         TestBed.configureTestingModule({
-            imports: [CoreModule],
+            imports: [CoreModule, MatDialogModule, MatRadioModule, ReactiveFormsModule, MatSelectModule, MatInputModule],
             declarations: [
                 DeviceTypesContentVariableDialogComponent
             ],
             providers: [
                 {provide: MatDialogRef, useValue: matDialogRefSpy},
                 {provide: ConceptsService, useValue: conceptServiceSpy},
-                {provide: MAT_DIALOG_DATA, useValue: {contentVariable: {} as DeviceTypeContentVariableModel, functions: [] as DeviceTypeFunctionModel[]}},
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {contentVariable: contentVariable, functions: [] as DeviceTypeFunctionModel[]}
+                },
             ]
         }).compileComponents();
         fixture = TestBed.createComponent(DeviceTypesContentVariableDialogComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+    }
+
+    beforeEach(async(() => {
     }));
 
     it('should create the app', async(() => {
+        const contentVariable: DeviceTypeContentVariableModel = {} as DeviceTypeContentVariableModel;
+        init(contentVariable);
         expect(component).toBeTruthy();
     }));
+
+    it('create primitive Type', async(() => {
+        init({} as DeviceTypeContentVariableModel);
+        expect(component.isPrimitiveType()).toBe(true);
+        expect(component.firstFormGroup.invalid).toBe(true);
+        component.firstFormGroup.patchValue({
+            name: 'testName',
+            type: component.primitiveTypes[0].type,
+        });
+        expect(component.firstFormGroup.valid).toBe(true);
+        component.firstFormGroup.patchValue({
+            name: 'testName',
+            type: component.primitiveTypes[0].type,
+            characteristic_id: 'characteristic_id',
+            serialization_options: ['serialization_options'],
+            unit_reference: 'unit_reference',
+            value: 'value',
+        });
+        expect(component.firstFormGroup.getRawValue()).toEqual({
+            id: null,
+            name: 'testName',
+            type: component.primitiveTypes[0].type,
+            characteristic_id: 'characteristic_id',
+            serialization_options: ['serialization_options'],
+            unit_reference: 'unit_reference',
+            sub_content_variables: null,
+            value: 'value',
+        });
+    }));
+
+    it('edit primitive Type', async(() => {
+        const contentVariable: DeviceTypeContentVariableModel = {
+            id: 'id1',
+            name: 'testName',
+            type: 'https://schema.org/Text',
+            characteristic_id: 'characteristic_id',
+            serialization_options: ['serialization_options'],
+            unit_reference: 'unit_reference',
+            value: 'value',
+        } as DeviceTypeContentVariableModel;
+        init(contentVariable);
+        expect(component.typeOptionsControl.disabled).toBe(true);
+        expect(component.firstFormGroup.getRawValue()).toEqual({
+            id: 'id1',
+            name: 'testName',
+            type: component.primitiveTypes[0].type,
+            characteristic_id: 'characteristic_id',
+            serialization_options: ['serialization_options'],
+            unit_reference: 'unit_reference',
+            sub_content_variables: null,
+            value: 'value',
+        });
+    }));
+
+    it('create non-primitive Type', async(() => {
+        const contentVariable: DeviceTypeContentVariableModel = {} as DeviceTypeContentVariableModel;
+        init(contentVariable);
+        component.typeOptionsControl.setValue('non-primitive');
+        expect(component.isPrimitiveType()).toBe(false);
+        expect(component.firstFormGroup.invalid).toBe(true);
+        component.firstFormGroup.patchValue({
+            name: 'testStruct',
+            type: component.nonPrimitiveTypes[0].type,
+        });
+        expect(component.firstFormGroup.valid).toBe(true);
+        expect(component.firstFormGroup.getRawValue()).toEqual({
+            id: null,
+            name: 'testStruct',
+            type: component.nonPrimitiveTypes[0].type,
+            characteristic_id: null,
+            serialization_options: null,
+            unit_reference: null,
+            sub_content_variables: [],
+            value: null,
+        });
+    }));
+
+    it('edit non-primitive Type', async(() => {
+        const contentVariable: DeviceTypeContentVariableModel = {
+            id: 'id2',
+            name: 'testStruct',
+            type: 'https://schema.org/StructuredValue',
+            sub_content_variables: [{
+                id: 'id3',
+                name: 'testFloat',
+                type: 'https://schema.org/Float'
+            }] as DeviceTypeContentVariableModel[],
+        } as DeviceTypeContentVariableModel;
+        init(contentVariable);
+        expect(component.typeOptionsControl.disabled).toBe(true);
+        expect(component.firstFormGroup.getRawValue()).toEqual({
+            id: 'id2',
+            name: 'testStruct',
+            type: 'https://schema.org/StructuredValue',
+            characteristic_id: null,
+            serialization_options: null,
+            unit_reference: null,
+            sub_content_variables: [
+                {
+                    id: 'id3',
+                    name: 'testFloat',
+                    type: 'https://schema.org/Float'
+                }
+            ],
+            value: null,
+        });
+    }));
+
 
 
 
--- src/app/modules/devices/device-types-overview/device-types/dialogs/device-types-content-variable-dialog.component.ts
+++ src/app/modules/devices/device-types-overview/device-types/dialogs/device-types-content-variable-dialog.component.ts
@@ -43,36 +43,46 @@ export class DeviceTypesContentVariableDialogComponent implements OnInit {
                 @Inject(MAT_DIALOG_DATA) data: { contentVariable: DeviceTypeContentVariableModel, functions: DeviceTypeFunctionModel[] }) {
         this.contentVariable = data.contentVariable;
         this.functions = data.functions;
-        this.initTypesList();
-
     }
 
     ngOnInit(): void {
+        this.initTypesList();
+        this.initFormGroup();
+        this.loadConcepts();
+        this.initTypeOptionControl();
+    }
+
+    close(): void {
+        this.dialogRef.close();
+    }
+
+    save(): void {
+        this.dialogRef.close(this.firstFormGroup.getRawValue());
+    }
+
+    isPrimitiveType(): boolean {
+        return this.typeOption === 'primitive';
+    }
+
+    private initTypeOptionControl() {
         if (this.contentVariable.type) {
             this.typeOptionsControl.disable();
-            if (this.contentVariable.type === 'https://schema.org/StructuredValue' || this.contentVariable.type === 'https://schema.org/ItemList') {
-                this.typeOptionsControl.setValue('non-primitive');
-            } else {
-                this.typeOptionsControl.setValue('primitive');
-            }
+        }
+        if (this.isPrimitiveType()) {
+            this.typeOptionsControl.setValue('primitive');
+        } else {
+            this.typeOptionsControl.setValue('non-primitive');
         }
 
         this.typeOptionsControl.valueChanges.subscribe(() => {
             this.firstFormGroup.reset();
+            this.firstFormGroup.patchValue({
+                sub_content_variables: this.isPrimitiveType() ? null : [],
+            });
         });
+    }
 
-        this.firstFormGroup = this._formBuilder.group({
-                id: [{value: this.contentVariable.id, disabled: true}],
-                name: [this.contentVariable.name],
-                type: [this.contentVariable.type],
-                characteristic_id: [this.contentVariable.characteristic_id],
-                serialization_options: [this.contentVariable.serialization_options],
-                unit_reference: [this.contentVariable.unit_reference],
-                value: [this.contentVariable.value],
-                typeOption: null,
-            }
-        );
-
+    private loadConcepts() {
         if (this.functions) {
             this.functions.forEach(
                 (func: DeviceTypeFunctionModel) => {
@@ -87,24 +97,18 @@ export class DeviceTypesContentVariableDialogComponent implements OnInit {
         }
     }
 
-    close(): void {
-        this.dialogRef.close();
-    }
-
-    save(): void {
-        const contentVariable: DeviceTypeContentVariableModel = this.firstFormGroup.getRawValue();
-
-        if (this.isPrimitiveType()) {
-            contentVariable.sub_content_variables = undefined;
-        } else {
-            contentVariable.sub_content_variables = [];
-        }
-
-        this.dialogRef.close(contentVariable);
-    }
-
-    isPrimitiveType(): boolean {
-        return this.typeOption === 'primitive';
+    private initFormGroup() {
+        this.firstFormGroup = this._formBuilder.group({
+                id: [{value: this.contentVariable.id || null, disabled: true}],
+                name: [this.contentVariable.name],
+                type: [this.contentVariable.type],
+                characteristic_id: [this.contentVariable.characteristic_id],
+                serialization_options: [this.contentVariable.serialization_options],
+                unit_reference: [this.contentVariable.unit_reference],
+                sub_content_variables: [this.contentVariable.sub_content_variables],
+                value: [this.contentVariable.value],
+            }
+        );
     }
 
     private initTypesList(): void {
@@ -112,11 +116,12 @@ export class DeviceTypesContentVariableDialogComponent implements OnInit {
         this.primitiveTypes.push({type: 'https://schema.org/Integer', typeShort: 'int'});
         this.primitiveTypes.push({type: 'https://schema.org/Float', typeShort: 'float'});
         this.primitiveTypes.push({type: 'https://schema.org/Boolean', typeShort: 'bool'});
+
         this.nonPrimitiveTypes.push({type: 'https://schema.org/StructuredValue', typeShort: 'Structure'});
         this.nonPrimitiveTypes.push({type: 'https://schema.org/ItemList', typeShort: 'List'});
     }
 
-    get typeOption() {
+    get typeOption(): string {
         return this.typeOptionsControl.value;
     }
 }
