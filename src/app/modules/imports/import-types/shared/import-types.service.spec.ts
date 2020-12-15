1948fdd6f40a8e7aa47f1ccbca831d42afacd295
franzmueller <franzmueller@users.noreply.github.com>
2020-12-15 08:32:27 +0100

    improve import types service tests

--- src/app/modules/imports/import-types/shared/import-types.service.spec.ts
+++ src/app/modules/imports/import-types/shared/import-types.service.spec.ts
@@ -16,21 +16,76 @@
 import {TestBed} from '@angular/core/testing';
 
 import {ImportTypesService} from './import-types.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {createSpyFromClass, Spy} from 'jasmine-auto-spies';
+import {of} from 'rxjs';
+import {ImportTypeModel} from './import-types.model';
+import {environment} from '../../../../../environments/environment';
 
 describe('ImportTypesService', () => {
     let service: ImportTypesService;
 
+    const httpClientSpy: Spy<HttpClient> = createSpyFromClass(HttpClient);
+    httpClientSpy.get.and.returnValue(of(null));
+    httpClientSpy.put.and.returnValue(of(null));
+    httpClientSpy.post.and.returnValue(of(null));
+    httpClientSpy.delete.and.returnValue(of(null));
+
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [
-                HttpClientModule,
+                HttpClientTestingModule,
             ],
+            providers: [
+                {provide: HttpClient, useValue: httpClientSpy},
+            ]
         });
         service = TestBed.inject(ImportTypesService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should correctly request lists', () => {
+        service.listImportTypes('search', 10, 1, 'name.asc')
+            .subscribe(val => expect(val).toEqual([]));
+        expect(httpClientSpy.get.calls.mostRecent().args[0]).toEqual(
+            environment.permissionSearchUrl + '/v2/import-types?&search=search&limit=10&offset=1&sort=name.asc'
+        );
+    });
+
+    it('should correctly request a single type', () => {
+        service.getImportType('1234');
+        expect(httpClientSpy.get.calls.mostRecent().args[0]).toEqual(
+            environment.importRepoUrl + '/import-types/1234'
+        );
+    });
+
+    it('should correctly create a type', () => {
+        const exampleType: ImportTypeModel = {id: '', name: 'test'} as ImportTypeModel;
+        service.saveImportType(exampleType);
+        expect(httpClientSpy.post.calls.mostRecent().args[0]).toEqual(
+            environment.importRepoUrl + '/import-types'
+        );
+        expect(httpClientSpy.post.calls.mostRecent().args[1]).toEqual(exampleType);
+    });
+
+    it('should correctly update a type', () => {
+        const exampleType: ImportTypeModel = {id: '1234'} as ImportTypeModel;
+        service.saveImportType(exampleType);
+        expect(httpClientSpy.put.calls.mostRecent().args[0]).toEqual(
+            environment.importRepoUrl + '/import-types/1234'
+        );
+        expect(httpClientSpy.put.calls.mostRecent().args[1]).toEqual(exampleType);
+    });
+
+    it('should correctly delete a type', () => {
+        service.deleteImportInstance('1234');
+        expect(httpClientSpy.delete.calls.mostRecent().args[0]).toEqual(
+            environment.importRepoUrl + '/import-types/1234'
+        );
     });
 });
