4aac038d3896c16c9954fe06c34e229d78a84499
Ingo <roessner@infai.org>
2022-05-16 09:19:27 +0200

    SNRGY-2052: fix chart export edit display name

--- src/app/widgets/charts/export/dialog/charts-export-edit-dialog.component.ts
+++ src/app/widgets/charts/export/dialog/charts-export-edit-dialog.component.ts
@@ -248,7 +248,7 @@ export class ChartsExportEditDialogComponent implements OnInit {
         })));
         obs.push(this.deviceInstancesService.getDeviceInstances('', 9999, 0, 'name', 'asc')
             .pipe(
-                map(this.deviceInstancesService.useDisplayNameAsName),
+                map(devices => this.deviceInstancesService.useDisplayNameAsName(devices)),
                 map(devices => devices as DeviceInstancesModel[]),
                 map(devices => this.deviceList = devices)));
         return forkJoin(obs);
