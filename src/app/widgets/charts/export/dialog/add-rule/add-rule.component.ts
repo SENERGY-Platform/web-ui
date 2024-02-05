063f902d1d4d918f211b29be212e6dba3370a3fe
Hannes Hansen <mail@hannesh.de>
2024-02-05 16:49:56 +0100

    fixed edit chart export conversion

--- src/app/widgets/charts/export/dialog/add-rule/add-rule.component.ts
+++ src/app/widgets/charts/export/dialog/add-rule/add-rule.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ChartsExportConversion } from '../../shared/charts-export-properties.model';
 
 @Component({
   selector: 'app-add-rule',
@@ -8,16 +9,24 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./add-rule.component.css']
 })
 export class AddRuleComponent {
-  constructor(
-    private dialogRef: MatDialogRef<AddRuleComponent>
-  ) {}
-
   form = new FormGroup({
     from: new FormControl('', {nonNullable: true, validators: Validators.required}),
     to: new FormControl('', {nonNullable: true, validators: Validators.required}),
     color: new FormControl(''),
     alias: new FormControl(''),
   });
+
+  constructor(
+    private dialogRef: MatDialogRef<AddRuleComponent>,
+    @Inject(MAT_DIALOG_DATA) public rule?: ChartsExportConversion,
+  ) {
+    if(rule != null) {
+      this.form.controls.from.patchValue(rule.from);
+      this.form.controls.to.patchValue(rule.to);
+      this.form.controls.color.patchValue(rule.color || '');
+      this.form.controls.alias.patchValue(rule.alias || '');
+    }
+  }
 
   cancel() {
     this.dialogRef.close();
