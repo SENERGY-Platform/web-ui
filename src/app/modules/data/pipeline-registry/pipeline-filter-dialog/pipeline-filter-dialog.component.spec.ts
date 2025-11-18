e205f272abd1931b841206b74f210ca795e6278e
Ingo <roessner@infai.org>
2025-11-18 10:43:20 +0100

    fix PipelineFilterDialogComponent test dependencies

--- src/app/modules/data/pipeline-registry/pipeline-filter-dialog/pipeline-filter-dialog.component.spec.ts
+++ src/app/modules/data/pipeline-registry/pipeline-filter-dialog/pipeline-filter-dialog.component.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PipelineFilterDialogComponent } from './pipeline-filter-dialog.component';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {createSpyFromClass, Spy} from 'jasmine-auto-spies';
+import {of} from 'rxjs';
+import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 
 describe('PipelineFilterDialogComponent', () => {
   let component: PipelineFilterDialogComponent;
   let fixture: ComponentFixture<PipelineFilterDialogComponent>;
 
+  const dialogSpy: Spy<MatDialog> = createSpyFromClass(MatDialog);
+  dialogSpy.open.and.returnValue({afterClosed: () => of(true)});
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PipelineFilterDialogComponent]
+        imports: [MatDialogModule],
+        declarations: [PipelineFilterDialogComponent],
+        providers: [
+            {provide: MatDialog, useValue: dialogSpy},
+            { provide: MatDialogRef, useValue: { close: jasmine.createSpy('close') } },
+            { provide: MAT_DIALOG_DATA, useValue: undefined },
+            provideHttpClient(withInterceptorsFromDi()),
+        ]
     })
     .compileComponents();
 
