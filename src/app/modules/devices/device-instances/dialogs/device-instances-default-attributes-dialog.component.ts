f53ffa3ad907788222a85477792ecb0a3c87dd79
franzmueller <franzmueller@users.noreply.github.com.>
2025-12-01 14:42:11 +0100

    improve default attributes

--- src/app/modules/devices/device-instances/dialogs/device-instances-default-attributes-dialog.component.ts
+++ src/app/modules/devices/device-instances/dialogs/device-instances-default-attributes-dialog.component.ts
@@ -17,17 +17,32 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { AddTagFn } from '@ng-matero/extensions/select';
-import { Attribute } from '../shared/device-instances.model';
 import { DeviceInstancesService } from '../shared/device-instances.service';
 import { catchError } from 'rxjs';
 import { ErrorHandlerService } from 'src/app/core/services/error-handler.service';
+import { Attribute } from '../shared/device-instances.model';
+
+export interface AttributeKeyWithDescription {
+    label?: string;
+    description?: string;
+    key: string;
+}
 
 @Component({
     templateUrl: './device-instances-default-attributes-dialog.component.html',
     styleUrls: ['./device-instances-default-attributes-dialog.component.css'],
 })
 export class DeviceInstancesDefaultAttributesDialogComponent implements OnInit {
-    knownAttributes = ['anomaly-detector', 'timezone', 'inactive', 'last_message_max_age', 'monitor_connection_state', 'platform/mute-format-error', 'senergy/snowflake-canary-device', 'senergy/canary-device'];
+    knownAttributes: AttributeKeyWithDescription[] = [
+        {key: 'anomaly-detector'},
+        {key: 'timezone'},
+        {key: 'inactive'},
+        {key: 'last_message_max_age', label: 'Offline Detection', description: 'Considers device as offline when no messages are received within the duration defined, e. g. "5m" for 5 minutes.'},
+        {key: 'monitor_connection_state', label: 'Offline Notification', description: 'Sends a notification when the device has been offline for the duration defined, e. g. "5m" for 5 minutes.'},
+        {key: 'platform/mute-format-error'},
+        {key: 'senergy/snowflake-canary-device'},
+        {key: 'senergy/canary-device'},
+    ];
     attributes: Attribute[] = [];
     ready = false;
 
@@ -42,6 +57,12 @@ export class DeviceInstancesDefaultAttributesDialogComponent implements OnInit {
     ngOnInit(): void {
         this.deviceInstancesService.getDefaultAttributes().subscribe(a => {
             this.attributes = a;
+            this.knownAttributes.filter(knownAttr => knownAttr.label !== undefined).forEach(knownAttr => {
+                const match = this.attributes.find(attr => attr.key === knownAttr.key);
+                if (!match) {
+                    this.attributes.push({ key: knownAttr.key, value: '', origin: this.deviceInstancesService.defaultOrigin } );
+                }
+            });
             this.ready = true;
         });
     }
@@ -51,7 +72,7 @@ export class DeviceInstancesDefaultAttributesDialogComponent implements OnInit {
     }
 
     save(): void {
-        this.deviceInstancesService.setDefaultAttributes(this.attributes)
+        this.deviceInstancesService.setDefaultAttributes(this.attributes.filter(attr => attr.value.trim() !== ''))
             .pipe(catchError(this.errorHandlerService.handleErrorWithSnackBar(DeviceInstancesDefaultAttributesDialogComponent.name, 'save', 'Error saving default attributes')))
             .subscribe(_ => {
                 this.dialogRef.close(true);
@@ -73,11 +94,19 @@ export class DeviceInstancesDefaultAttributesDialogComponent implements OnInit {
     addAttribute(): AddTagFn {
         const that = this;
         return (text: string) => {
-            that.knownAttributes.push(text);
+            that.knownAttributes.push({key: text});
             const tmp = [...that.knownAttributes];
-            tmp.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+            tmp.sort((a, b) => a.key.toLowerCase().localeCompare(b.key.toLowerCase()));
             that.knownAttributes = [];
             that.knownAttributes = tmp;
         };
+    }
+
+    isDisabled(key: string): boolean {
+        return this.knownAttributes.find(a => a.key === key && a.label !== undefined) !== undefined;
+    }
+
+    tooltip(key: string): string|undefined {
+        return this.knownAttributes.find(a => a.key === key)?.description;
     }
 }
