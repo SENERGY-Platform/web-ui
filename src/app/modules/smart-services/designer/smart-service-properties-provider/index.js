8d18ce148246e4718defd197d326c273acade29f
Ingo <roessner@infai.org>
2026-01-30 08:41:10 +0100

    disable smart-service done event creation

--- src/app/modules/smart-services/designer/smart-service-properties-provider/index.js
+++ src/app/modules/smart-services/designer/smart-service-properties-provider/index.js
@@ -20,6 +20,9 @@ const typeBoolean = "https://schema.org/Boolean";
 const typeList = "https://schema.org/ItemList";
 const typeStructure = "https://schema.org/StructuredValue"; */
 
+
+const ADD_DONE_EVENT = false;
+
 function SmartServicePropertiesProvider(eventBus, canvas, bpmnFactory, elementFactory, autoPlace, elementRegistry, elementTemplates, bpmnjs, replace, selection, modeling, translate) {
     this.getTabs = function(element) {
         var camunda = new CamundaProvider(eventBus, canvas, bpmnFactory, elementRegistry, elementTemplates, translate);
@@ -150,22 +153,25 @@ function createTaskEntries(group, element, bpmnjs, eventBus, bpmnFactory, elemen
                     setExtentionsElement(bpmnjs, startElement.businessObject, formData);
                 }
 
-                let doneEventName = taskId+"_done_event";
-                let doneEventNameRef = "${"+doneEventName+"}";
-                let doneEvent = findMessageEventElement(element, doneEventNameRef);
+                if (ADD_DONE_EVENT) {
+                    let doneEventName = taskId+"_done_event";
+                    let doneEventNameRef = "${"+doneEventName+"}";
+                    let doneEvent = findMessageEventElement(element, doneEventNameRef);
 
-                let usesWaitParam = function (prefix) {
-                    return !!taskInfo.inputs.find(value => {
-                        return value.name === prefix+".wait" && value.value === "true"
-                    })
+                    let usesWaitParam = function (prefix) {
+                        return !!taskInfo.inputs.find(value => {
+                            return value.name === prefix+".wait" && value.value === "true"
+                        })
+                    }
+
+                    if (taskInfo.topic === "process_deployment" && !doneEvent){
+                        createIntermediateCatchMessageEvent(bpmnFactory, elementFactory, autoPlace, replace, element, doneEventNameRef)
+                    }
+                    if (taskInfo.topic === "device_repository" && !doneEvent && !usesWaitParam("device_repository")){
+                        createIntermediateCatchMessageEvent(bpmnFactory, elementFactory, autoPlace, replace, element, doneEventNameRef)
+                    }
                 }
-                
-                if (taskInfo.topic === "process_deployment" && !doneEvent){
-                    createIntermediateCatchMessageEvent(bpmnFactory, elementFactory, autoPlace, replace, element, doneEventNameRef)
-                }
-                if (taskInfo.topic === "device_repository" && !doneEvent && !usesWaitParam("device_repository")){
-                    createIntermediateCatchMessageEvent(bpmnFactory, elementFactory, autoPlace, replace, element, doneEventNameRef)
-                }
+
 
                 toExternalServiceTask(bpmnFactory, replace, selection, element, function (serviceTask, element) {
                     serviceTask.topic = taskInfo.topic;
@@ -198,8 +204,10 @@ function createTaskEntries(group, element, bpmnjs, eventBus, bpmnFactory, elemen
                     switch (serviceTask.topic){
                         case "process_deployment":
                             outputs.push(createTextOutputParameter(bpmnjs, taskId+"_process_deployment_id", "${process_deployment_id}"));
-                            outputs.push(createTextOutputParameter(bpmnjs, doneEventName, "${done_event}"));
                             let fogInput = inputs.find(value => value.name === "process_deployment.prefer_fog_deployment");
+                            if (ADD_DONE_EVENT){
+                                outputs.push(createTextOutputParameter(bpmnjs, doneEventName, "${done_event}"));
+                            }
                             if(fogInput && fogInput.value === "true") {
                                 outputs.push(createTextOutputParameter(bpmnjs, taskId+"_is_fog_deployment", "${is_fog_deployment}"));
                                 outputs.push(createTextOutputParameter(bpmnjs, taskId+"_fog_hub", "${fog_hub}"));
@@ -217,7 +225,9 @@ function createTaskEntries(group, element, bpmnjs, eventBus, bpmnFactory, elemen
                         case "device_repository":
                             outputs.push(createTextOutputParameter(bpmnjs, taskId+"_device_group_id", "${device_group_id}"));
                             outputs.push(createTextOutputParameter(bpmnjs, taskId+"_device_group_selection", "${device_group_iot_option}"));
-                            outputs.push(createTextOutputParameter(bpmnjs, doneEventName, "${done_event}"));
+                            if (ADD_DONE_EVENT){
+                                outputs.push(createTextOutputParameter(bpmnjs, doneEventName, "${done_event}"));
+                            }
                             break;
                     }
 
